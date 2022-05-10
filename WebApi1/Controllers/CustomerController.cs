364c53f47a3081e953f703cd4f0799de34884c96
Xeli_hx11@itstep.edu.az <Xeli_hx11@itstep.edu.az>
2022-05-10 11:28:16 +0400

    Add project files.

---
+++ WebApi1/Controllers/CustomerController.cs
@@ -0,0 +1,128 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApi1.DataAccess;
+using WebApi1.Entities;
+
+namespace WebApi1.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private ICustomerDal _customerDal;
+
+        public CustomerController(ICustomerDal customerDal)
+        {
+            _customerDal = customerDal;
+        }
+
+        [HttpGet("")]
+        public IActionResult Get()
+        {
+            var customers = _customerDal.GetList();
+            return Ok(customers);
+        }
+
+
+        [HttpGet("{customerId}")]
+        public IActionResult Get(string customerId)
+        {
+            try
+            {
+                var customer = _customerDal.Get(p => p.CustomerId == customerId);
+                if (customer == null)
+                {
+                    return NotFound($"There is not customer with id : {customerId}");
+                }
+                return Ok(customer);
+            }
+            catch (Exception)
+
+            {
+
+            }
+            return BadRequest();
+        }
+
+
+        [HttpPost]
+        public IActionResult Post([FromForm] Customer customer)
+        {
+            try
+            {
+                _customerDal.Add(customer);
+                return new StatusCodeResult(200);
+            }
+            catch (Exception)
+            {
+            }
+            return BadRequest();
+        }
+
+
+        [HttpPut]
+        public IActionResult Put(Customer customer)
+        {
+            try
+            {
+                _customerDal.Update(customer);
+                return Ok();
+            }
+            catch (Exception)
+            {
+            }
+            return BadRequest();
+        }
+
+        [HttpDelete("{customerId}")]
+        public IActionResult Delete(string customerId)
+        {
+            try
+            {
+                _customerDal.Delete(new Customer { CustomerId = customerId });
+                return Ok();
+            }
+            catch (Exception)
+            {
+
+            }
+            return BadRequest();
+        }
+
+
+        [HttpGet("Orders")]
+        public IActionResult GetOrdersByCustomerId(string customerId)
+        {
+            try
+            {
+                var result = _customerDal.GetOrdersByCustomerId(customerId);
+                return Ok(result);
+            }
+            catch (Exception)
+            {
+
+            }
+            return BadRequest();
+        }
+
+        [HttpGet("Alphabetic")]
+        public IActionResult GetCustomersByOrderby(bool isAlphabetic)
+        {
+            try
+            {
+                var result=_customerDal.GetCustomersByOrderBy(isAlphabetic);
+                return Ok(result);
+            }
+            catch (Exception)
+            {
+
+            }
+            return BadRequest();
+        }
+
+      
+
+
+
+    }
+}
---
+++ WebApi1/DataAccess/NorthwindContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApi1.Entities;
+
+namespace WebApi1.DataAccess
+{
+    public class NorthwindContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string conn = @"Data Source=(localdb)\ProjectModels;Initial Catalog=Northwind;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            optionsBuilder.UseSqlServer(conn);
+        } 
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
